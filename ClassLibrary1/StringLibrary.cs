4d0db742889d8b99fed04bd74af3a2d0ca4bd5c4
Khaled Alrusan <khaled66658032@gmail.com>
2023-08-07 22:00:44 -0400

    Modified files name

--- ClassLibrary1/StringLibrary.cs
+++ ClassLibrary1/StringLibrary.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    public static class Class1
+    public static class StringLibrary
     {
         public static bool StartsWithUpper(this string str)
         {
